8aa552cec525a9aaf0a270f00b2b7823a8590b03
Likhitha <likhithah2005@gmail.com>
2026-04-17 14:21:17 +0530

    react-app

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import Todoinput from "./components/Todoinput"
+
+function App(){
+  return(
+    <>
+    <Header/>
+    <Todoinput/>
+  </>
+  )
+}
+export default App
